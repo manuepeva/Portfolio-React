d1dbdaedcc280507afbfd9297d1e79a6203eaae4
ManuDeveloper <manuepeva@gmail.com>
2020-08-26 08:39:25 -0500

    Updates

---
+++ precache-manifest.016467c4bd5825d7b73c0d4b2c1ca14d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7168f4aabf20e8c9ed75c6206c5d9e75",
+    "url": "/manuepeva/Portfolio-Reactindex.html"
+  },
+  {
+    "revision": "26206f870addce44595c",
+    "url": "/manuepeva/Portfolio-Reactstatic/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3a7f95f577eb321ad718",
+    "url": "/manuepeva/Portfolio-Reactstatic/css/main.3f052e4c.chunk.css"
+  },
+  {
+    "revision": "26206f870addce44595c",
+    "url": "/manuepeva/Portfolio-Reactstatic/js/2.5ac64799.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/manuepeva/Portfolio-Reactstatic/js/2.5ac64799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7f95f577eb321ad718",
+    "url": "/manuepeva/Portfolio-Reactstatic/js/main.3f1212fb.chunk.js"
+  },
+  {
+    "revision": "00dec6cdfa2fc9671cb3",
+    "url": "/manuepeva/Portfolio-Reactstatic/js/runtime-main.a45d683c.js"
+  },
+  {
+    "revision": "a8ee56d51ab9278322c34490614f3402",
+    "url": "/manuepeva/Portfolio-Reactstatic/media/CVDeutsch.a8ee56d5.pdf"
+  },
+  {
+    "revision": "86854e23a75e8596f4e2557ce3d934bc",
+    "url": "/manuepeva/Portfolio-Reactstatic/media/CVEN.86854e23.pdf"
+  },
+  {
+    "revision": "3861111c67f50ef537519ee8f8464455",
+    "url": "/manuepeva/Portfolio-Reactstatic/media/CuscoAfternoon.3861111c.jpeg"
+  },
+  {
+    "revision": "7386cda1344ea983e48e96a1d09685ca",
+    "url": "/manuepeva/Portfolio-Reactstatic/media/ManuelP.7386cda1.jpg"
+  }
+]);
